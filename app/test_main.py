9ad1fffc69111cea80602dcc78dcdb7c4596b3fd
Darryl Morton <darrylmorton@users.noreply.github.com>
2023-09-24 14:56:09 +0100

    trio dep, test cleanup

--- app/test_main.py
+++ app/test_main.py
@@ -6,7 +6,7 @@ from .main import app
 
 @pytest.mark.anyio
 async def test_health():
-    async with AsyncClient(app=app, base_url="http://loclahost:8001") as ac:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/health")
     assert response.status_code == 200
     assert response.json() == {"message": "ok"}
