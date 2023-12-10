c1872be46dd4b6fc3dfab6c81943e62e7d90cc39
darrylmorton <darryl.j.morton@gmail.com>
2023-12-10 08:04:23 +0000

    test data

--- tests/integration/routers/users_route_test.py
+++ tests/integration/routers/users_route_test.py
@@ -5,8 +5,8 @@ from tests.helper.routes import http_client, TEST_URL, http_post_client
 
 # TODO return id and username from save result...
 async def test_post_user(db_cleanup):
-    username = "foo2"
-    payload = json.dumps({"username": username, "password": "bar2"})
+    username = "foo"
+    payload = json.dumps({"username": username, "password": "bar"})
 
     response = await http_post_client(TEST_URL, "/api/users", payload)
     actual_result = response.json()
@@ -16,7 +16,7 @@ async def test_post_user(db_cleanup):
 
 
 async def test_get_users():
-    expected_result = "foo2"
+    expected_result = "foo"
 
     response = await http_client(TEST_URL, "/api/users")
     actual_result = response.json()
@@ -27,7 +27,7 @@ async def test_get_users():
 
 
 async def test_get_by_user_username():
-    expected_result = "foo2"
+    expected_result = "foo"
 
     response = await http_client(TEST_URL, f"/api/users/{expected_result}")
     actual_result = response.json()
