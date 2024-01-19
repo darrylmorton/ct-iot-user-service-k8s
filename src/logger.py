080bd3c143151b064db84506f55fae44bfe98aef
darrylmorton <darrylmorton@users.noreply.github.com>
2024-01-19 00:20:21 +0000

    remove class (#3)

--- src/logger.py
+++ src/logger.py
@@ -2,40 +2,20 @@ import logging
 import structlog
 
 
-class ServiceLogger:
-    def __init__(self):
-        structlog.configure(
-            processors=[
-                # structlog.contextvars.merge_contextvars,
-                # structlog.processors.add_log_level,
-                # structlog.processors.StackInfoRenderer(),
-                structlog.dev.set_exc_info,
-                structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=True),
-                structlog.processors.dict_tracebacks,
-                structlog.processors.JSONRenderer(),
-            ],
-            wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
-            context_class=dict,
-            logger_factory=structlog.PrintLoggerFactory(),
-            cache_logger_on_first_use=False,
-        )
+structlog.configure(
+    processors=[
+        # structlog.contextvars.merge_contextvars,
+        # structlog.processors.add_log_level,
+        # structlog.processors.StackInfoRenderer(),
+        structlog.dev.set_exc_info,
+        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=True),
+        structlog.processors.dict_tracebacks,
+        structlog.processors.JSONRenderer(),
+    ],
+    wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
+    context_class=dict,
+    logger_factory=structlog.PrintLoggerFactory(),
+    cache_logger_on_first_use=False,
+)
 
-        self.log = structlog.get_logger()
-
-    def warning(self, message: str):
-        self.log.warning(message)
-
-    def debug(self, message: str):
-        self.log.debug(message)
-
-    def info(self, message: str):
-        self.log.info(message)
-
-    def error(self, message: str):
-        self.log.error(message)
-
-    def critical(self, message: str):
-        self.log.critical(message)
-
-
-log = ServiceLogger()
+log = structlog.get_logger()
