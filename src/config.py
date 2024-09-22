e3ced53e8ac12359bdf96b10ead2a91fad064518
darrylmorton <darryl.j.morton@gmail.com>
2024-09-22 20:36:22 +0100

    cleanup

--- src/config.py
+++ src/config.py
@@ -21,11 +21,12 @@ LOG_LEVEL = os.environ.get("LOG_LEVEL")
 SERVICE_NAME = os.environ.get("SERVICE_NAME")
 APP_PORT = os.environ.get("UVICORN_PORT") or 8001
 
-AUTH_SERVICE_URL = os.environ.get("AUTH_SERVICE_URL")
-AUTH_SERVICE_URL = f"{AUTH_SERVICE_URL}/api"
+AUTH_SERVICE_ENDPOINT = os.environ.get("AUTH_SERVICE_URL")
+AUTH_SERVICE_URL = f"{AUTH_SERVICE_ENDPOINT}/api"
 
 DB_USERNAME = os.environ.get("DB_USERNAME")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_PASSWORD_PARSED = quote(DB_PASSWORD).replace("%", "%%")
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
@@ -34,7 +35,7 @@ DATABASE_URL_PREFIX = "postgresql+asyncpg"
 DATABASE_URL_SUFFIX = (
     "{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}".format(
         DB_USERNAME=DB_USERNAME,
-        DB_PASSWORD=quote(DB_PASSWORD).replace("%", "%%"),
+        DB_PASSWORD=DB_PASSWORD_PARSED,
         DB_HOST=DB_HOST,
         DB_PORT=DB_PORT,
         DB_NAME=DB_NAME,
