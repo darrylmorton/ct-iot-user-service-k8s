fd70129c28afa317c546e619c8b632ef12e62f3a
darrylmorton <darryl.j.morton@gmail.com>
2024-10-23 00:53:45 +0100

    version bump

--- src/database/config.py
+++ src/database/config.py
@@ -9,9 +9,3 @@ from config import DATABASE_URL
 async_engine = create_async_engine(DATABASE_URL, future=True, echo=False)
 async_session = async_sessionmaker(async_engine, expire_on_commit=False, autoflush=True)
 Base = declarative_base()
-
-
-# TODO try to use this or remove
-# async def get_db() -> AsyncSession:
-#     async with async_session() as session:
-#         yield session
