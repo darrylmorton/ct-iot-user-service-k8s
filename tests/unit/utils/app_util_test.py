7f582d889513fbde5f014ef58a41ff466fe66762
darrylmorton <darryl.j.morton@gmail.com>
2024-10-29 11:11:28 +0000

    version bump

--- tests/unit/utils/app_util_test.py
+++ tests/unit/utils/app_util_test.py
@@ -20,7 +20,7 @@ class TestAppUtil:
     async def test_get_app_version(self):
         actual_result = AppUtil.get_app_version()
 
-        assert actual_result == "1.0.2"
+        assert actual_result == "1.0.3"
 
     @skip
     def test_create_db_url_suffix(self):
