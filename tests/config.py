07e73891afa06ae199b70a9d8ce002011c6b2e5f
darrylmorton <darryl.j.morton@gmail.com>
2025-12-27 14:12:56 +0000

    chore: test.env file

--- tests/config.py
+++ tests/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from utils.app_util import AppUtil
 
 
-load_dotenv(dotenv_path=".env")
+load_dotenv(dotenv_path="test.env")
 
 AWS_REGION = os.environ.get("AWS_REGION")
 USERNAME = os.environ.get("USERNAME")
